1d59babbe6165ade75d2acb7289705df7d211495
Gourang Tiwari <tiwarigourang2006@gmail.com>
2026-09-11 04:04:05 +0530

    Time: 1 ms (81.33%), Space: 42.9 MB (78.62%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,36 +1,25 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        Stack<Character>st1=new Stack<>();
-        Stack<Character>st2=new Stack<>();
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='#'){
-            if(!st1.isEmpty()){    
-            st1.pop();
-            }
+        return helper(s).equals(helper(t));
+    }
+    public String helper(String hello){
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<hello.length();i++){
+            char ch=hello.charAt(i);
+            if(ch=='#'){
+                if(sb.length()>0)
+                sb.deleteCharAt(sb.length()-1);
+
             }
             else{
-            st1.push(s.charAt(i));
-        }
-    }
-        for(int i=0;i<t.length();i++){
-            if(t.charAt(i)=='#'){
-                if(!st2.isEmpty()){    
-            st2.pop();
+                sb.append(ch);
             }
-        }else{
-
-        
-            st2.push(t.charAt(i));
-
+           
         }
-        }
-       while (!st1.isEmpty() && !st2.isEmpty()) {
-            if (st1.pop() != st2.pop()) {
-                return false;
-            }
-        }
-
-        
-        return st1.isEmpty() && st2.isEmpty();
+         return sb.toString();
     }
+        
+        
+
 }
+    
